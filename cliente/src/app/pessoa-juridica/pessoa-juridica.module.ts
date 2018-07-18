7e99f45aa3131276c50b3078dae25c440b53ad82
Janser <janser.ferreira@basis.com.br>
2018-07-18 17:06:13 -0300

    BASIS-44109 - Listar Pessoa jurídica

--- cliente/src/app/pessoa-juridica/pessoa-juridica.module.ts
+++ cliente/src/app/pessoa-juridica/pessoa-juridica.module.ts
@@ -1,14 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { pessoaJuridicaRoute } from './pessoa-juridica.route';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PessoaJuridicaListComponent } from './pessoa-juridica-list/pessoa-juridica-list.component';
 import { RouterModule } from '@angular/router';
+import { ButtonModule, DataTableModule } from 'primeng/primeng';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(pessoaJuridicaRoute)
+    RouterModule.forChild(pessoaJuridicaRoute),
+    FormsModule,
+    ButtonModule,
   ],
   declarations: [PessoaJuridicaListComponent]
 })
