a613836d91627c8a6e576168c484a33b22e59e86
Janser <janser.ferreira@basis.com.br>
2018-07-24 15:56:06 -0300

    BASIS-44106 - Cadastro de pessoa jurídica

--- cliente/src/app/util/mensagens-util.ts
+++ cliente/src/app/util/mensagens-util.ts
@@ -4,8 +4,8 @@ export class MensagensUtils {
     REGISTRO_SALVO ="Salvo com Sucesso.";
     PREENCHA_CAMPOS_OBRIGATORIOS ="Preencha os campos obrigatórios.";
     CAMPO_OBRIGATORIO ="Campo obrigatório.";
-    CARREGANDO="Carregando";
-    SALVANDO="Salvando";
+    CARREGANDO="Carregando...";
+    SALVANDO="Salvando...";
     CNPJ_INVALIDO ="CNPJ inválido."
     
 
