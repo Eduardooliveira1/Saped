0cbb8df884c275dae0f323325471dce03148079b
Janser <janser.ferreira@basis.com.br>
2018-08-02 16:23:43 -0300

    BASIS-44089 -  login

--- servico/src/main/java/br/gov/mme/service/dto/UsuarioDTO.java
+++ servico/src/main/java/br/gov/mme/service/dto/UsuarioDTO.java
@@ -1,4 +1,26 @@
 package br.gov.mme.service.dto;
 
-public class UsuarioDTO {
+import java.io.Serializable;
+
+public class UsuarioDTO implements Serializable {
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public UsuarioDTO setUsuario(String usuario) {
+        this.usuario = usuario;
+        return this;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public UsuarioDTO setSenha(String senha) {
+        this.senha = senha;
+        return this;
+    }
 }
