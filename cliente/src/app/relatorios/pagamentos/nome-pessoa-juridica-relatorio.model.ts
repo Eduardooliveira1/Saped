b0c727e845d89aaef18c7fa645505485fa0f7770
breno.alves <breno.alves@basis.com.br>
2018-08-08 13:24:50 -0300

    ADD: Parcial Adicionando função do relatório: listagem de pagamentos:  - BASIS-44112

---
+++ cliente/src/app/relatorios/pagamentos/nome-pessoa-juridica-relatorio.model.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+@Injectable()
+class NomePessoaJuridicaRelatorio {
+    id: number;
+    nome: string;
+    constructor(id: number, nome: string) {
+        this.id = id;
+        this.nome = nome;
+    }
+    getNomesPessoasJuridicas
+}
