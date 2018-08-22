8514296a77ababd1e28380c420320bddcd43fe00
breno.alves <breno.alves@basis.com.br>
2018-08-22 12:37:32 -0300

    ADD: Adicionando função do relatório: listagem de pagamentos:  - BASIS-44112

--- cliente/src/app/util/custom-utils.ts
+++ cliente/src/app/util/custom-utils.ts
@@ -1,10 +1,9 @@
 import { SelectItem } from 'primeng/primeng';
 
 export class CustomUtils {
-
   public static CAMPO_VALOR_PADRAO = 'id';
   public static CAMPO_LABEL_PADRAO = 'descricao';
-  public static  CAMPO_PESSOA_JURIDICA_PADRAO = 'nome';
+  public static CAMPO_PESSOA_JURIDICA_PADRAO = 'nome';
   public static CAMPO_ANO_REFERENCIA = 'ano';
 
   public static entityToDropDown(list: any[], label, value): SelectItem[] {
