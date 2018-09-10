b2e225d589538368449a698e794cacb8ee92911c
breno.alves <breno.alves@basis.com.br>
2018-09-10 11:21:34 -0300

    ADD: Adicionando função do relatório: listagem de pagamentos:  - BASIS-44112: Correção máscara exibição monetária.

--- cliente/src/app/shared/custom-pipes/dot-to-comma.pipe.ts
+++ cliente/src/app/shared/custom-pipes/dot-to-comma.pipe.ts
@@ -3,6 +3,6 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'dotToComma' })
 export class DotToCommaPipe implements PipeTransform {
   transform(value: string) {
-    return value.replace('.', ',' );
+    return value.replace(/,/g, '.' ).replace(/.([^.]+)$/g, ',$1');
   }
 }
